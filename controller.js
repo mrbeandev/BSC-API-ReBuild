0913075e9ee8f672346d841e4e8674a38e9624e5
Tulasinathreddy <34623886+Tulasinathreddy@users.noreply.github.com>
2021-07-16 16:04:39 +0530

    Update controller.js

--- controller.js
+++ controller.js
@@ -9,7 +9,7 @@ async function createAccount(req, res) {
         let account = await web3.eth.accounts.create();
         res.status(200).send({ status: true, account });
     } catch(error) {
-        res.status(500).send({ status: false, message: 'Create Account Failed' });
+        res.status(500).send({ status: false, message: 'Create Account Failed', error : error.message });
     }
 }
 
@@ -21,7 +21,7 @@ async function getBalance(req, res) {
         let balance = await web3.eth.getBalance(req.body.address);
         res.status(200).send({ status: true, balance: web3.utils.fromWei(balance, 'ether') });
     } catch(error) {
-        res.status(500).send({ status: false, message: 'Get BNB Balance Failed' });
+        res.status(500).send({ status: false, message: 'Get BNB Balance Failed', error : error.message });
     }
 }
 
@@ -31,12 +31,12 @@ async function getTokenBalance(req, res) {
 
     try {
         // contract instance
-        const contract = new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
+        const contract = new web3.eth.Contract(abi, req.body.contract);
         const balance = await contract.methods.balanceOf(req.body.address).call();
         const decimals = await contract.methods.decimals().call();
         res.status(200).send({ status: true, balance: balance / 10**decimals });
     } catch(error) {
-        res.status(500).send({ status: false, message: 'Get Token Balance Failed' });
+        res.status(500).send({ status: false, message: 'Get Token Balance Failed', error : error.message });
     }
 }
 
@@ -59,7 +59,7 @@ async function transfer(req, res) {
         
         res.status(200).send({ status: true, hash: tx.transactionHash });
     } catch (error) {
-        res.status(500).send({ status: false, message: 'Transfer Failed' });
+        res.status(500).send({ status: false, message: 'Transfer Failed', error : error.message });
     }
 }
 
@@ -69,14 +69,14 @@ async function transferToken(req, res) {
 
     try {
         // contract instance
-        const contract = await new web3.eth.Contract(abi, process.env.CONTRACT_ADDRESS);
+        const contract = await new web3.eth.Contract(abi, req.body.contract);
         const decimals = await contract.methods.decimals().call();
         // transfer event abi
         const transferAbi = await contract.methods.transfer(req.body.to, (req.body.amount * 10**decimals).toString()).encodeABI();
 
         // Sign transaction
         let signTransaction = await web3.eth.accounts.signTransaction({
-            to: process.env.CONTRACT_ADDRESS,
+            to: req.body.contract,
             data: transferAbi,
             gas: req.body.gas || 2000000
         }, req.body.from_private_key);
@@ -88,7 +88,7 @@ async function transferToken(req, res) {
         
         res.status(200).send({ status: true, hash: tx.transactionHash });
     } catch (error) {
-        res.status(500).send({ status: false, message: 'Transfer Failed' });
+        res.status(500).send({ status: false, message: 'Transfer Failed' , error : error.message });
     }
 }
 
